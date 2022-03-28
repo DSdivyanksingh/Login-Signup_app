dd713a235188c7130ce5e9cdcd544b6635ce83db
divyank12singh <divyanksingh1297@gmail.com>
2022-03-28 19:56:03 +0530

    submission

---
+++ fontend/login_signup_app/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+import Home from './components/home/home';
+import Signup from './components/signup/signup';
+import Login from './components/login/login';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from 'react';
+
+
+function App() {
+
+  const [ user, setLoginUser] = useState({})
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {
+              user && user._id ? <Home setLoginUser={setLoginUser} /> : <Login setLoginUser={setLoginUser}/>
+            }
+          </Route>
+          <Route path="/login">
+            <Login setLoginUser={setLoginUser}/>
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
